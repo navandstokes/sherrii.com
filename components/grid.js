100818f2aa1c967b3507b510f83723cc2810e6a8
Sherri Cui <sherrii.cui@pm.me>
2019-09-11 20:58:00 +0800

    grid transitions

--- components/grid.js
+++ components/grid.js
@@ -3,12 +3,21 @@ import Link from 'next/link'
 export const Grid = props => {
 	const Items = props.items.fields.items.map((item) => {
 		return (
-			<div className="w-100 w-50-m w-25-l" key={item.fields.title}>
+			<div className="w-100 w-50-m w-25-l overflow-hidden" key={item.fields.title}>
+			<style jsx>{`
+				img {
+					transition: all 0.3s ease;
+				}
+				img:hover {
+					transform: scale(1.2) rotate(0deg);
+					opacity: 0.5;
+				}
+			`}</style>
 				<Link href={'page?slug=' + item.fields.slug}
 					as={'/p/'+ item.fields.slug}
 					passHref>
 					<a>
-						<div className="aspect-ratio aspect-ratio--1x1 cover dim pointer" >
+						<div className="aspect-ratio aspect-ratio--1x1 cover pointer" >
 							<img src={item.fields.images[0].fields.file.url + '?fit=fill&w=480&h=480'} alt={item.fields.images[0].fields.title} />
 							<div className="absolute w-100 h-100 flex flex-column justify-center content-center">
 								<p className="tc ma0">{item.fields.title}</p>
@@ -22,11 +31,6 @@ export const Grid = props => {
 	})
 	return (
 		<div className="w-100 flex flex-row flex-wrap">
-		<style jsx>{`
-			a::hover div {
-				
-			}
-		`}</style>
 			{Items}
 		</div>
 	)
