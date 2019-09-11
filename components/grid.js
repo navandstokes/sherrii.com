a5ebd9869f90ed081fc7cf698a87a150c120e678
Sherri Cui <sherrii.cui@pm.me>
2019-09-11 21:10:39 +0800

    grid hover css

--- components/grid.js
+++ components/grid.js
@@ -7,22 +7,29 @@ export const Grid = props => {
 			<style jsx>{`
 				img {
 					transition: all 0.3s ease;
+					z-index: -1;
 				}
-				img:hover {
+				a:hover img {
 					transform: scale(1.2) rotate(0deg);
 					opacity: 0.5;
+				}
+				a div {
+					opacity: 0;
+					transition: all 0.3s ease;
+					transition-delay: 0.1s;
+				}
+				a:hover div {
+					opacity: 0.75;
 				}
 			`}</style>
 				<Link href={'page?slug=' + item.fields.slug}
 					as={'/p/'+ item.fields.slug}
 					passHref>
-					<a>
-						<div className="aspect-ratio aspect-ratio--1x1 cover pointer" >
-							<img src={item.fields.images[0].fields.file.url + '?fit=fill&w=480&h=480'} alt={item.fields.images[0].fields.title} />
-							<div className="absolute w-100 h-100 flex flex-column justify-center content-center">
-								<p className="tc ma0">{item.fields.title}</p>
-								<p className="i tc ma0">-view-</p>
-							</div>
+					<a className="db pointer relative">
+						<img className="db" src={item.fields.images[0].fields.file.url + '?fit=fill&w=480&h=480'} alt={item.fields.images[0].fields.title} />
+						<div className="absolute top-0 left-0 w-100 h-100 flex flex-column justify-center content-center white">
+							<p className="tc ma0">{item.fields.title}</p>
+							<p className="i tc ma0">-view-</p>
 						</div>
 					</a>
 				</Link>
