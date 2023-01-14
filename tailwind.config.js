6787990a480a4a4c87c71fef6bd0a23e6da4f376
navandstokes <sherrii.cui@pm.me>
2023-01-14 15:01:49 +0100

    feat: add twitter and github

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
         "./app/**/*.{js,ts,jsx,tsx}",
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
-        "!./node_modules",
     ],
     theme: {
         extend: {},
