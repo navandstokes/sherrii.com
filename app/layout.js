8ce9e86c31bd66effe91da4e4bbb41b31b9620ad
navandstokes <sherrii.cui@pm.me>
2023-01-26 16:18:15 +0100

    feat: add umami with env variables

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,14 @@
 import "lib/globals.css"
+import Script from "next/script"
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body>{children}</body>
+            <Script
+                data-website-id={process.env.NEXT_PUBLIC_UMAMI_ID}
+                src={process.env.NEXT_PUBLIC_UMAMI_WEBSITE}
+            />
         </html>
     )
 }
